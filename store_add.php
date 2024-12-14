75dde8524c90e3162dc5d423c60e370be16ff488
liyue0731 <andrew.virtual01@gmail.com>
2024-12-14 22:41:53 +0800

    po

--- store_add.php
+++ store_add.php
@@ -3,15 +3,15 @@
     if (empty($_POST['name'])) {
         die('請輸入商店名稱');
     }
-    $stName         = $_POST["name"];
-    $stPhone        = $_POST["phone_number"];
-    $stWorkTime     = $_POST["work_time"];
-    $stAddress      = $_POST["address"];
+    $name         = $_POST["name"];
+    $phone_number        = $_POST["phone_number"];
+    $work_time     = $_POST["work_time"];
+    $address      = $_POST["address"];
 
     $query = ("Insert into store values(?,?,?,?)");
     $stmt = $db->prepare($query);
     $stmt->bind_param("ssss", $name, $phone_number, $work_time, $address);
     $stmt->execute();
 
-    header('Location: store_edit.php');
+    header('Location: store.php');
 ?>
